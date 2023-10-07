123469dbcb862078130f5b8c46c4f7dda0178518
Tom Collins <tom.collins@moxiworks.com>
2023-10-07 13:54:23 +1100

    Fix typo: Should be val= (not just val)

--- lib/fitreader/message.rb
+++ lib/fitreader/message.rb
@@ -33,7 +33,7 @@ class Message
       val = Sdk.enum(type[:type])[val]
     elsif type[:type] == :date_time
       t = Time.new(1989, 12, 31, 0, 0, 0, '+00:00').utc.to_i
-      val Time.at(val + t).utc
+      val = Time.at(val + t).utc
     elsif type[:type] == :local_date_time
       t = Time.new(1989, 12, 31, 0, 0, 0, '+02:00').utc.to_i
       val = Time.at(val + t)
